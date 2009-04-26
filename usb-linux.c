677fa91e376b98d871bb238ecb84685dd4f33ebd
Hector Martin <hector@marcansoft.com>
2009-04-26 20:33:29 +0200

    Hello, world!

---
+++ usb-linux.c
@@ -0,0 +1,30 @@
+/*
+	usbmuxd - iPhone/iPod Touch USB multiplex server daemon
+
+Copyright (C) 2009	Hector Martin "marcan" <hector@marcansoft.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 or version 3.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+
+#include <libusb.h>
+
