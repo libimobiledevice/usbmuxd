8bd54babbe6d3309f78f9d8729d93f83c14d23f5
Nikias Bassen <nikias@gmx.li>
2009-09-08 14:21:11 +0200

    Fix compile under Darwin with more includes, also silences some warnings.

--- libusbmuxd/sock_stuff.c
+++ libusbmuxd/sock_stuff.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <errno.h>
 #include <sys/time.h>
--- tools/iproxy.c
+++ tools/iproxy.c
@@ -23,6 +23,7 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <stddef.h>
 #include <sys/socket.h>
