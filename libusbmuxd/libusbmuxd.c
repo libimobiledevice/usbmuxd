e52f09541559aec86bd43bb0a914bafc9153c2de
Hector Martin <hector@marcansoft.com>
2010-01-23 23:07:07 +0100

    Add missing pthread.h include to libusbmuxd

--- libusbmuxd/libusbmuxd.c
+++ libusbmuxd/libusbmuxd.c
@@ -30,6 +30,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <signal.h>
+#include <pthread.h>
 
 // usbmuxd public interface
 #include "usbmuxd.h"
