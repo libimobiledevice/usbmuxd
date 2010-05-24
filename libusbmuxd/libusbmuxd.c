dfc1c8653b4da6086e6c241f50503e9a011066d4
Nikias Bassen <nikias@gmx.li>
2010-05-24 17:45:33 +0200

    Use more common predefined compiler macro to detect win32

--- libusbmuxd/libusbmuxd.c
+++ libusbmuxd/libusbmuxd.c
@@ -67,7 +67,7 @@ static usbmuxd_device_info_t *devices_find(int handle)
  */
 static int connect_usbmuxd_socket()
 {
-#ifdef WINDOWS
+#ifdef WIN32
 	return connect_socket("127.0.0.1", 27015);
 #else
 	return connect_unix_socket(USBMUXD_SOCKET_FILE);
