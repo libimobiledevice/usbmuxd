fca7a9c0f0dfd22d3cabe646fc6f49f14858c1d0
kobybo <koby.b@mce-sys.com>
2014-01-21 09:33:27 +0200

    Fixed vcxproj settings

--- libusbmuxd/usbmuxd-proto.h
+++ libusbmuxd/usbmuxd-proto.h
@@ -29,7 +29,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #define USBMUXD_PROTOCOL_VERSION 0
 
 #if defined(WIN32) || defined(__CYGWIN__)
-#define USBMUXD_SOCKET_PORT 27015
+#define USBMUXD_SOCKET_PORT 37015//27015
 #else
 #define USBMUXD_SOCKET_FILE "/var/run/usbmuxd"
 #endif
