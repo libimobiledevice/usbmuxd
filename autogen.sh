4e8740a05e820777edee97f1c6531aeecd467e96
Martin Szulecki <m.szulecki@libimobiledevice.org>
2014-10-16 15:11:10 +0200

    Add a pkg-config requirement check in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -9,6 +9,10 @@ aclocal -I m4
 autoheader
 automake --add-missing
 autoconf
+requires_pkgconfig=`which pkg-config 2>&1 >/dev/null`
+if [ $? -ne 0 ]; then
+  echo "Missing required pkg-config. Please install it on your system and run again."
+fi
 
 if [ -z "$NOCONFIGURE" ]; then
     ./configure "$@"
