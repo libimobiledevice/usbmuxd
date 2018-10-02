49576fdf06e3a7e3ef7824f29136556005f2239d
Nikias Bassen <nikias@gmx.li>
2018-10-02 14:54:18 +0200

    preflight: Fix compatibility with latest internal changes to libimobiledevice

--- src/preflight.c
+++ src/preflight.c
@@ -49,8 +49,10 @@ enum connection_type {
 
 struct idevice_private {
 	char *udid;
+	uint32_t mux_id;
 	enum connection_type conn_type;
 	void *conn_data;
+	int version;
 };
 
 struct cb_data {
@@ -123,8 +125,10 @@ static void* preflight_worker_handle_device_add(void* userdata)
 	struct device_info *info = (struct device_info*)userdata;
 	struct idevice_private *_dev = (struct idevice_private*)malloc(sizeof(struct idevice_private));
 	_dev->udid = strdup(info->serial);
+	_dev->mux_id = info->id;
 	_dev->conn_type = CONNECTION_USBMUXD;
-	_dev->conn_data = (void*)(long)info->id;
+	_dev->conn_data = NULL;
+	_dev->version = 0;
 
 	idevice_t dev = (idevice_t)_dev;
 
