28ddeccdfed5a9d7bd23cf73442343972009fddb
Martin Szulecki <martin.szulecki@libimobiledevice.org>
2020-06-09 03:49:01 +0200

    Apply changes towards a project wide common "autogen.sh" file

--- autogen.sh
+++ autogen.sh
@@ -1,19 +1,31 @@
 #!/bin/sh
-gprefix=`which glibtoolize 2>&1 >/dev/null`
-if [ $? -eq 0 ]; then 
-  glibtoolize --force
-else
-  libtoolize --force
-fi
-aclocal -I m4
-autoheader
-automake --add-missing
-autoconf
-requires_pkgconfig=`which pkg-config 2>&1 >/dev/null`
-if [ $? -ne 0 ]; then
-  echo "Missing required pkg-config. Please install it on your system and run again."
-fi
+
+olddir=`pwd`
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+(
+  cd "$srcdir"
+
+  gprefix=`which glibtoolize 2>&1 >/dev/null`
+  if [ $? -eq 0 ]; then
+    glibtoolize --force
+  else
+    libtoolize --force
+  fi
+  aclocal -I m4
+  autoheader
+  automake --add-missing
+  autoconf
+
+  requires_pkgconfig=`which pkg-config 2>&1 >/dev/null`
+  if [ $? -ne 0 ]; then
+    echo "Missing required pkg-config. Please install it on your system and run again."
+  fi
+
+  cd "$olddir"
+)
 
 if [ -z "$NOCONFIGURE" ]; then
-    ./configure "$@"
+  $srcdir/configure "$@"
 fi
