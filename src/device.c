243dedf14357b097aa638ce4e02dde48133a52ef
Imre Ehreth <imre.ehreth@gmail.com>
2024-06-13 11:23:35 +0200

    device: Fix crash caused by wrong variable when logging an error

--- src/device.c
+++ src/device.c
@@ -661,7 +661,7 @@ static void device_tcp_input(struct mux_device *dev, struct tcphdr *th, unsigned
 		if(!(th->th_flags & TH_RST)) {
 			usbmuxd_log(LL_INFO, "No connection for device %d incoming packet %d->%d", dev->id, dport, sport);
 			if(send_anon_rst(dev, sport, dport, ntohl(th->th_seq)) < 0)
-				usbmuxd_log(LL_ERROR, "Error sending TCP RST to device %d (%d->%d)", conn->dev->id, sport, dport);
+				usbmuxd_log(LL_ERROR, "Error sending TCP RST to device %d (%d->%d)", dev->id, sport, dport);
 		}
 		return;
 	}
